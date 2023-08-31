a9a01e9c52acb998e68c74374312224022d871bb
Роман Носов <romannosov_2001@mail.ru>
2023-08-31 17:55:50 +0300

    First commit

--- src/main/java/ru/netology/SpringBootAuthorization/repository/UserRepository.java
+++ src/main/java/ru/netology/SpringBootAuthorization/repository/UserRepository.java
@@ -3,9 +3,16 @@ package ru.netology.SpringBootAuthorization.repository;
 import ru.netology.SpringBootAuthorization.controller.Authorities;
 
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class UserRepository {
+
+    private final ConcurrentHashMap<String, String> data = new ConcurrentHashMap<>(Map.of("Ivan", "1111", "Peter", "2222",
+            "Alex", "3333", "Gordon", "4444"));
+
     public List<Authorities> getUserAuthorities(String user, String password) {
+
         return null;
     }
 }
